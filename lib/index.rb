38aa6b1a7302c2c6b4480983ff1743c5fab0a057
ja9-look <janineluk95@gmail.com>
2018-12-06 12:09:17 +0000

    added 3 methods into index

--- lib/index.rb
+++ lib/index.rb
@@ -7,24 +7,32 @@ def search_techcrunch
   response_hash = JSON.parse(response_string.body)
 end
 
-# def search_by_author(author)
-#   search_full_api.find do |info| info["articles"][0]["name"] == author
-#   end
-# end
+#shows a list of all authors and articles
+def show_all_articles_with_authors
+  @all = {}
+  search_techcrunch["articles"].each do |hash|
+    author = hash["author"]
+    title = hash["title"]
+    @all[title] = author
+  end
+  @all.each do |title, author| puts "#{author} - #{title}"
+  end
+  return nil
+end
 
-# def find_techcrunch(author)
-#   response_string = RestClient.get"https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=45aee5b7c7584064ac1b1de6297f5137"
-#   response_hash = JSON.parse(response_string.body)
-# end
-#
-#
-# def find_article(article)
-#   Set.all.find do |article|
-#     article.name == article
-#   end
-# end
+#shows all authors in alphabetical order
+def show_all_authors
+  @all = []
+  search_techcrunch["articles"].each do |article|
+    @all << article["author"]
+  end
+  unique_array = @all.uniq!.sort
+  unique_array.each_with_index do |author, index| puts "#{index+1}. #{author}"
+  end
+  return nil
+end
 
-
+#finds all articles under the given author name (partial - first or last)
 def find_article_titles_by_author(author)
   @titles = []
   search_techcrunch["articles"].each do |article|
@@ -35,6 +43,24 @@ def find_article_titles_by_author(author)
   @titles.each_with_index do |title, index| puts "#{index+1}. #{title}"
   end
   return nil
+end
+
+#shows the latest article
+def show_latest_article
+  author = search_techcrunch["articles"][0]["author"]
+  title = search_techcrunch["articles"][0]["title"]
+  content = search_techcrunch["articles"][0]["content"]
+  publish_time = search_techcrunch["articles"][0]["publishedAt"]
+  url = search_techcrunch["articles"][0]["url"]
+  puts "#{title}\n\n#{content}\n\n#{author} - #{publish_time}"
+  puts "\nWould you like to read the full article? (y/n)"
+  user_input = gets.chomp
+    if user_input == "y"
+      system("open #{url}")
+    else
+      puts "\nNot your cuppa tea!? Do any of the following articles interest you?\n\n"
+      show_all_articles_with_authors
+    end
 end
 
 # def find_article_by_phrase(phrase)
